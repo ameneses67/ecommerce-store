4705cef4e2ddd70ca1dae65afb087a18eac42cc5
Poncho <ameneses67@gmail.com>
2023-12-02 22:37:04 -0600

    Store environment setup & featured products

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,28 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const relative = 0;
+
+const HomePage = async () => {
+	const billboard = await getBillboard("55e93555-c707-431f-9538-613fd9ef426e");
+	const products = await getProducts({ isFeatured: true });
+
+	return (
+		<Container>
+			<div className="space-y-10 pb-10">
+				<Billboard data={billboard} />
+				<div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+					<ProductList
+						title="Productos Destacados"
+						items={products}
+					/>
+				</div>
+			</div>
+		</Container>
+	);
+};
+export default HomePage;
